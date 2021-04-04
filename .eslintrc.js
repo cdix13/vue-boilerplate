58d98b7f74af030c5e2172b55bb5a3344dd7602f
Sidik <sidik.saepudin@mekari.com>
2021-04-04 11:39:16 +0700

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,28 @@
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
   root: true,
   env: {
     node: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "@vue/prettier",
+    "prettier",
+    "prettier/vue",
+  ],
   parserOptions: {
     parser: "babel-eslint",
   },
+  plugins: ["vue", "prettier", "vuetify"],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-console": isProd ? "error" : "off",
+    "no-debugger": isProd ? "error" : "off",
+    "vuetify/no-deprecated-classes": "error",
+    // 'no-param-reassign': 'off',
+    "import/no-extraneous-dependencies": "off",
+    "prettier/prettier": "error",
   },
   overrides: [
     {
